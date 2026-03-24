b6752a202876c29ccef0e79e0fdd825e32190f3f
ykamio3872-max <ykamio3872@gmail.com>
2026-03-24 21:14:53 +0900

    feat: edit entrypoint.sh

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# srcディレクトリが空、かつ laravel が未インストールの場合のみ実行
-if [ ! -f "artisan" ]; then
+# srcコンテナに.gitkeepのみ存在するときに実行
+if [ -z "$(ls -A /var/www/html | grep -v .gitkeep)"]; then
     composer create-project --prefer-dist "laravel/laravel=8.*" .
     # 必要に応じて権限を変更
     chmod -R 777 storage bootstrap/cache
